5eb2cfcb12ca635f946339415e0db83bc7eb666c
alexslk <alexdneprin@icloud.com>
2023-04-26 17:35:10 +0100

    Bob added some changes

--- iOS Discord project/ViewController.swift
+++ iOS Discord project/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        topLabel.text = "Hey!"
+    }
+    
+    func myFunc() {
+        
     }
 
 
